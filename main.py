080e52a96fabacbc140c7e7ed023e21682be09f4
ArielTriana <usich37@gmail.com>
2021-04-07 16:32:25 -0400

    Adding simulator singleton pattern design

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from simulator import Simulator
-from initializer import Initializer
+from simulator_singleton import Simulator_Singleton
 
 if __name__ == "__main__":
     # Give me an instance of the simulator class. This instance will 
@@ -8,31 +7,28 @@ if __name__ == "__main__":
     # the simulation times, the signal time, the pending instructions, 
     # the instructions that have to be executed in a time and it will 
     # also handle the data sending
-    
-    init = Initializer()
-    init.load_config()
-    
-    simulator = Simulator(init.get("signal_time"), init.get("script_name"))
-    del init
+
+      
 
     while True:
+
         # update the instructions that must be executed in the current simulation time
-        simulator.update_instructions()
+        Simulator_Singleton.instance().update_instructions()
         
         # clear devices 
-        simulator.clear_network_component()
+        Simulator_Singleton.instance().clear_network_component()
 
         # execute pending data sendings
-        simulator.execute_sending_device()
-        simulator.send_switch()
+        Simulator_Singleton.instance().execute_sending_device()
+        Simulator_Singleton.instance().send_switch()
 
         # execute the instructions of this time
-        simulator.execute_time_instructions()
-        simulator.read_host_wire()
+        Simulator_Singleton.instance().execute_time_instructions()
+        Simulator_Singleton.instance().read_host_wire()
 
         # check if the simulation stop condition was reached
-        if simulator.must_stop():
+        if Simulator_Singleton.instance().must_stop():
             break
         
         #then advance simulation time 
-        simulator.advance_simulation()
+        Simulator_Singleton.instance().advance_simulation()
